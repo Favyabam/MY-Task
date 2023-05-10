0a80a80785f7b3fae83082191f03d109882010d8
Favyabam <favourdickens82@yahoo.com>
2023-05-10 10:25:40 +0100

    favour first commit

---
+++ src/app/mock-task.ts
@@ -0,0 +1,32 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctor Appointment',
+    day: 'feb 14',
+    reminder: true,
+    time: '10:00am'
+  },
+  {
+    id: 2,
+    text: 'meeting val',
+    day: 'feb 14',
+    reminder: true,
+    time: '12:15pm'
+  },
+  {
+    id: 3,
+    text: 'getting gifts for val',
+    day: 'feb 14',
+    reminder: false,
+    time: '17:00pm'
+  },
+  {
+    id: 4,
+    text: 'get voter card',
+    day: 'feb 18',
+    reminder: false,
+    time: '13:30pm'
+  }
+]
